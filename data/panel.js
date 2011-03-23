ec3b13505bf9a174f1bb29493664e29f139e5362
Paul Everitt <paul@agendaless.com>
2011-03-23 14:01:18 -0400

    Switched to a panel-oriented approach.  You click the widget and get a panel.  The panel has a button to do a manual sync and a button to toggle the (now functional) background sync.
    Also included a span where the addon can inform the panel about its last activity.

---
+++ data/panel.js
@@ -0,0 +1,35 @@
+var last_update = 'No updates yet';
+
+$('#kb-manual-sync').click(function () {
+    postMessage({type: 'trigger_sync'});
+});
+
+$('#kb-toggle-sync').click(function () {
+    postMessage({type: 'toggle_sync'});
+});
+
+onMessage = function onMessage(message) {
+    // Handle messages sent from the add-on.  For example, when
+    // a background file update happens, the panel might get
+    // sent information to display.
+    // Message look like this:
+    //   message.type - The kind of message
+    //   message.value - The data for that type of message
+    switch (message.type) {
+        case 'last_update': {
+            console.log('sent a last_update type');
+            $('#kb-last-updated').text(message.value);
+            return;
+        }
+        case 'sync_on': {
+            $('#kb-toggle-sync').text('Sync is ON')
+            console.log('sent a sync_on type')
+            return;
+        }
+        case 'sync_off': {
+            $('#kb-toggle-sync').text('Sync is ON')
+            console.log('sent a sync_off type')
+            return;
+        }
+    }
+}
