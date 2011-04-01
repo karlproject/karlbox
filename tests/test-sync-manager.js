a1f130a758a636be9e094dcf5593356f2708665c
Paul Everitt <paul@agendaless.com>
2011-04-01 11:21:14 -0400

    Still not finished, tests are broken, but want Balazs to see the design with the comments.  It isn't wired in yet so not a big deal to commit

--- tests/test-sync-manager.js
+++ tests/test-sync-manager.js
@@ -53,3 +53,39 @@ exports.test_sync_list_exists = function(test) {
     test.assert(sm.SyncList);
 };
 
+exports.test_sync_list_stored = function(test) {
+    var sl = new sm.SyncList({"aaa.txt": 999});
+    test.assert("aaa.txt" in sl.sync_map);
+    ;
+};
+
+exports.test_sync_list_initial_queue = function(test) {
+    var sl = new sm.SyncList({});
+    test.assert(sl.pending_queue.length == 0);
+};
+
+exports.test_sync_list_push_scenarios = function(test) {
+    // Various initial and post-initial states of the queue
+
+    // Bootstrap
+    SetupTestDir();
+    var sl = new sm.SyncList({});
+
+    // At this point, the Karlbox has never run, but has
+    // files in the kb directory that need syncing.
+    var sample_filenames = ['aaa.txt', 'bbb.txt', 'ccc.txt'];
+    sample_filenames.forEach(function(filename) {
+        var full_fn = file.join(base_dir, filename);
+        var f = new sm.SyncFile(full_fn);
+        sl.push(f);
+    });
+
+    // Now do some tests
+    var full_fn_1 = file.join(base_dir, "aaa.txt");
+    var full_fn_2 = file.join(base_dir, "XYZ.txt");
+    var qi_1 = sl.sync_map[full_fn_1];
+    var qi_2 = sl.sync_map[full_fn_2];
+    test.assert(qi_1, "Fn1 " + full_fn_1 + " not in syncmap");
+    test.assert(!qi_2, "Fn2 " + full_fn_2 + " wrongly in syncmap");
+test.assertEqual(qi_1.status == "added");   
+};
