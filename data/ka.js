388906c740f6f15d3ff71504ff9cf14e82fe1a9e
Paul Everitt <paul@agendaless.com>
2011-04-26 08:22:54 -0400

    The basics of the appswitcher UI are in there.  Not wired into the add-on yet.  Will probably get the Agility/Piledriver slickgrid in before moving into add-on.

--- data/ka.js
+++ data/ka.js
@@ -1,9 +1,30 @@
+
+function switchApp (app_id) {
+    // Change styling on app menu, hide existing panel, show new
+    var tgt = $('#ka-appswitcher a[href$="' + app_id + '"');
+    $('.ka-currentapp').removeClass('ka-currentapp');
+    tgt.addClass('ka-currentapp');
+    $('#ka-header-appname').text(app_id);
+    var tgt_panel = "ka-panel-" + app_id;
+    $('.ka-panel').each(function (i, elem) {
+        if ($(this).attr("id") == tgt_panel) {
+            $(this).css("display", "block");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+    $('#ka-narrowsearch').text(app_id);
+
+}
+
 $(document).ready(function() {
 
-    $('#ka-global-bg')
-            .globalbackground({
-                message: 'Howdy World'
-            });
+    $(".ka-form-button").button().css("margin", "0.2em");
 
-
+    // Hook up the app switcher at the top
+    $('#ka-appswitcher').click(function (evt) {
+        var tgt = $(evt.target);
+        var app_id = tgt.attr("href").slice(1);
+        switchApp(app_id);
+    })
 });
